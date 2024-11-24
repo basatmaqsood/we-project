eb228a698278ac52853b51e62c430da6fa17e516
basat <itzbasatmaqsood@gmail.com>
2024-11-24 13:42:48 +0500

    Working condition

---
+++ backend/routes/bookings.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { createBooking, getAllBooking, getBooking } from '../Controllers/bookingController.js'
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js'
+
+const router = express.Router()
+
+router.post('/', verifyToken, createBooking)
+router.get('/:id', verifyUser, getBooking)
+router.get('/', verifyAdmin, getAllBooking)
+
+export default router
